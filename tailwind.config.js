f778adc8d1f0a006d779288e7739b462e84a950e
arturconrado <45801765+arturconrado@users.noreply.github.com>
2021-03-20 00:05:47 -0300

    init

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./src/**/*.html', './src/**/*.scss'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
